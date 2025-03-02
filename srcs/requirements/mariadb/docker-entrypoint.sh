c78e8be8a97cd9c57907ac94f137ffbbb30830b7
Nao <133973097+dxe58709@users.noreply.github.com>
2025-03-02 14:50:57 +0000

    update

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -3,3 +3,26 @@
 if [ ! -d /var/lib/mysql/mysql ]; then
 	echo "Initializing MariaDB data directory..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    TEMP_FILE='/tmp/mysql-first-time.sql'
+	if [ ! -e /run/secrets/mysql_root_password ]; then
+		echo >&2 'Error: database is uninitialized MYSQL_ROOT_PASSWORD not set'
+		exit 1
+	fi
+	cat <<- EOSQL > "$TEMP_FILE"
+		DELETE FROM mysql.user;
+		CREATE USER 'root'@'%' IDENTIFIED BY '$(cat /run/secrets/mysql_root_password)';
+		GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
+		DROP DATABASE IF EXISTS test;
+	EOSQL
+    if [ -n "$MYSQL_DATABASE" ]; then
+		echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" >> "$TEMP_FILE"
+	fi
+	if [ -n "$MYSQL_USER" ] && [ -e /run/secrets/mysql_password ]; then
+		echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$(cat /run/secrets/mysql_password)';" >> "$TEMP_FILE"
+		if [ "$MYSQL_DATABASE" ]; then
+			echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> "$TEMP_FILE"
+		fi
+	fi
+	echo 'FLUSH PRIVILEGES;' >> "$TEMP_FILE"
+	set -- "$@" --init-file="$TEMP_FILE"
+fi
